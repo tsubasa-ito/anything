23f99b3d571222eb3612a7d6e5c41fad98785ec0
伊藤翼 <tsubasa8770@gmail.com>
2019-10-03 02:33:55 +0900

    heroku push

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,8 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
+        Schema::defaultStringLength(191);
         }
     }
 
